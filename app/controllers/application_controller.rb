a25da13b88dac22d84f584cd011787c5929fe4a8
“Allan <elgarallan@gmail.com>
2025-02-10 19:37:19 +0800

    user registration

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  before_action :set_current_user
+
+  def set_current_user
+    current_user
+  end
+
+  def authenticate_user
+    unless current_user
+      redirect_to root_path
+    end
+  end
+
+  def current_user
+    @current_user ||=
+      begin
+        return nil unless session[:user_id]
+
+        User.find(session[:user_id])
+      end
+  end
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    before_action :authenticate_user, only: :dashboard
+  
+    def welcome 
+    end
+  
+    def dashboard
+    end
+  
+  end
